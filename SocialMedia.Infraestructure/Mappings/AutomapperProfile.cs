a42c3468e0cb1d404d90dc7ee027d8906e8ce9b2
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-20 14:39:15 -0600

    Mapings AutomapperProfile
    CreateMap<Post, PostDto>();
    CreateMap<PostDto, Post>()

---
+++ SocialMedia.Infraestructure/Mappings/AutomapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SocialMedia.Core.DTOs;
+using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialMedia.Infraestructure.Mappings
+{
+    public class AutomapperProfile : Profile     //Definimos clase publica que hereda de la clase Profile
+    {
+        public AutomapperProfile()      //definimos constructor
+        {
+            CreateMap<Post, PostDto>();
+            CreateMap<PostDto, Post>();
+        }
+    };
+}
