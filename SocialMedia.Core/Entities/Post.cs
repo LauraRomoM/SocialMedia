44cfadc8499f7b211f9e26db98caa8c10f92573d
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-20 13:56:17 -0600

    misc Entity Post
    Intercambiando orden de atributos

--- SocialMedia.Core/Entities/Post.cs
+++ SocialMedia.Core/Entities/Post.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace SocialMedia.Core.Entities
@@ -9,14 +9,15 @@ namespace SocialMedia.Core.Entities
         {
             Coments = new HashSet<Coment>();
         }
-
-        public int UserId { get; set; }
+                
         public int PostId { get; set; }
+        public int UserId { get; set; }
         public DateTime Date { get; set; }
-        public string Descrition { get; set; }
+        public string Description { get; set; }
         public string Image { get; set; }
 
         public virtual User User { get; set; }
         public virtual ICollection<Coment> Coments { get; set; }
     }
 }
+  
