917e5992ee3120daa6506c54f085e2b215172395
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-23 22:58:31 -0600

    Api.Response ApiResponse (inicia clase)

---
+++ SocialMedia.Api/Responses/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Api.Responses
+{
+    //esta clase siempre se va a retornar en los CRUD a traves de nuestra APi,
+    public class ApiResponse
+    {
+    }
+}
