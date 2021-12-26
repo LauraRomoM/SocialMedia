dde9a69475451088d6fc73d95c86e0ab19b88fb0
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-26 16:13:23 -0600

    Core.Entities BaseEntity (agrega llave primaria (id) comúnn a las demas entidades)
    .

--- SocialMedia.Core/Entities/BaseEntity.cs
+++ SocialMedia.Core/Entities/BaseEntity.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,6 @@ namespace SocialMedia.Core.Entities
 {
     public abstract class BaseEntity        //abstracta y que no se generaran instancias de esta clase, solo se va a heredar de ella
     {
+        public int Id { get; set; }         //llave primaria id, (como todas nuestras entidades tienen llave primaria de tipo int, colocamos que esa llave se llame id)
     }
 }
