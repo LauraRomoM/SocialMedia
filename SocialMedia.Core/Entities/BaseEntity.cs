e4a67175e35576667a3b94190c78a6438aa3cad5
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-26 16:00:40 -0600

    Core.Entities BaseEntity (inicia clase)

---
+++ SocialMedia.Core/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialMedia.Core.Entities
+{
+    public abstract class BaseEntity        //abstracta y que no se generaran instancias de esta clase, solo se va a heredar de ella
+    {
+    }
+}
