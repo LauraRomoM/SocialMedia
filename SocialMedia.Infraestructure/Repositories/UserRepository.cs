bf2c65e1c704a2c4286ddbda2012816168ef962b
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-26 13:07:15 -0600

    Infraestructure.Repositories IUserRepository (add using and :IUserRepository)

--- SocialMedia.Infraestructure/Repositories/UserRepository.cs
+++ SocialMedia.Infraestructure/Repositories/UserRepository.cs
@@ -1,11 +1,12 @@
-﻿using SocialMedia.Infraestructure.Data;
+using SocialMedia.Infraestructure.Data;
+using SocialMedia.Core.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SocialMedia.Infraestructure.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
         private readonly SocialMediaContext _context;
         public UserRepository(SocialMediaContext context)
