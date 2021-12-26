b5cdeb2e30e62060b7d59cd3fd358ed3c8eab504
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-26 09:51:06 -0600

    Core.Services PostService (inicia clase y using)

---
+++ SocialMedia.Core/Services/PostService.cs
@@ -0,0 +1,16 @@
+﻿using SocialMedia.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialMedia.Core.Services
+{
+    public class PostService
+    {
+        private readonly IPostRepository _postRepository;
+        public PostService(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+    }
+}
