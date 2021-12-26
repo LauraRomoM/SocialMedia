e0268dd2b617c4f94aa06aa4c8933c37677d433a
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-26 12:02:40 -0600

    Infraestructure.Repositories UserRepository (inicia clase)

---
+++ SocialMedia.Infraestructure/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using SocialMedia.Infraestructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialMedia.Infraestructure.Repositories
+{
+    public class UserRepository
+    {
+        private readonly SocialMediaContext _context;
+        public UserRepository(SocialMediaContext context)
+        {
+            _context = context;
+        }
+    }
+}
