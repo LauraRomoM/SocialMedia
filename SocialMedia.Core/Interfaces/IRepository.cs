6d48e96075fb2935f895af67bf4a41fd3a65de55
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-26 17:58:50 -0600

    Core.Interfaces IRepository<T>(inicia interface)

---
+++ SocialMedia.Core/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Core.Interfaces
+{
+    public interface IRepository<T>  where T : BaseEntity       //clase de tipo de datos T que debe ser o heredar de la clase BaseEntity
+    {
+
+    }
+}
