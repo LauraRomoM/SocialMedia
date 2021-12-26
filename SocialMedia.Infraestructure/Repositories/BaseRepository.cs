0cfd20f042b99850d1ba5cfc03bb32dbfad2115f
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-26 17:59:51 -0600

    Infraestructure.Repositories BaseRepository<T> (inicia clase)

---
+++ SocialMedia.Infraestructure/Repositories/BaseRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialMedia.Infraestructure.Repositories
+{
+    public class BaseRepository<T> : IRepository<T>
+    {
+
+    }
+}
