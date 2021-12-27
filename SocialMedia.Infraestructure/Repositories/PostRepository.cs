280d51719c872bc695c5b3dc93415d3d92f314ef
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-27 13:41:56 -0600

    Infraestructure.Repositories PostRepository (add usings and body of class)

--- SocialMedia.Infraestructure/Repositories/PostRepository.cs
+++ SocialMedia.Infraestructure/Repositories/PostRepository.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infraestructure.Data;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SocialMedia.Infraestructure.Repositories
 {
-    class PostRepository
+    public class PostRepository : BaseRepository<Post>, IPostRepository
     {
+        public PostRepository(SocialMediaContext context) : base(context) { }  //enviamos el context a la clase padre a traves del metodo base()
+        
+        public async Task<IEnumerable<Post>> GetPostsByUser(int userId)
+        {
+            return await _entities.Where(x => x.UserId == userId).ToListAsync();
+        }
     }
 }
