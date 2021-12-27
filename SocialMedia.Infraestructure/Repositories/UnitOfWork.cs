b1eedb9b572e492ea535080b6453709e428315c2
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-26 21:04:14 -0600

    Infraestructure.Repositories UnitOfWork (inicia implementacion de interfaces)

--- SocialMedia.Infraestructure/Repositories/UnitOfWork.cs
+++ SocialMedia.Infraestructure/Repositories/UnitOfWork.cs
@@ -1,13 +1,38 @@
-﻿using SocialMedia.Infraestructure.Data;
+﻿using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infraestructure.Data;
+using System;
+using System.Threading.Tasks;
 
 namespace SocialMedia.Infraestructure.Repositories
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
         private readonly SocialMediaContext _context;
         public UnitOfWork(SocialMediaContext context)
         {
             _context = context;
+        }
+
+        public IRepository<Post> PostRepository => throw new NotImplementedException();
+
+        public IRepository<User> UserRepository => throw new NotImplementedException();
+
+        public IRepository<Coment> ComentRepository => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
