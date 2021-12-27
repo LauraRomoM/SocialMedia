0c73da60b7dd5c302f3632eb0027726becf1fe25
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-26 20:53:01 -0600

    Infraestructure.Repositories UnitOfWork (inicia clase)

---
+++ SocialMedia.Infraestructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace SocialMedia.Infraestructure.Repositories
+{
+    public class UnitOfWork 
+    {
+        public UnitOfWork()
+        {
+        
+        }
+    }
+}
