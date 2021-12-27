a69e5c26723aeefcc2019dc88b6ca78d1b04564a
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-27 16:58:50 -0600

    Infraestructure.Filters GlobalExceptioFilter (hereda de IExceptionFilter y implementa OnException())

---
+++ SocialMedia.Infraestructure/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Text;
+
+namespace SocialMedia.Infraestructure.Filters
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+
+        }
+    }
+}
