e59ad3d2259f05e45ca48b2ed4b7b030ec58fef7
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-26 20:22:36 -0600

    Infraestructure.Repositories BaseRepository<T> (usings, lectura de entidades y estructura del constructor)
    .

--- SocialMedia.Infraestructure/Repositories/BaseRepository.cs
+++ SocialMedia.Infraestructure/Repositories/BaseRepository.cs
@@ -1,11 +1,22 @@
-﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infraestructure.Data;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SocialMedia.Infraestructure.Repositories
 {
-    public class BaseRepository<T> : IRepository<T>
+    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
     {
+        private readonly SocialMediaContext _context;
+        private readonly DbSet<T> _entities;
+        public BaseRepository(SocialMediaContext context)       //constructor
+        {
+            _context = context;
+            _entities = context.Set<T>();       //Registramos matriculamos la entidad de tipo T (este puede ser Post, Update, Delete, etc)
+        }
 
     }
 }
