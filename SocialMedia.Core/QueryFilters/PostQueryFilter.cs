161be2887f312e6700a8622d5ce190873ee7304a
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-28 13:15:18 -0600

    Core.QueryFilters PostQueryFilter (agregando parametros al objeto)
    .

--- SocialMedia.Core/QueryFilters/PostQueryFilter.cs
+++ SocialMedia.Core/QueryFilters/PostQueryFilter.cs
@@ -7,5 +7,8 @@ namespace SocialMedia.Core.QueryFilters
     public class PostQueryFilter
     {
 
+        public int? UserId { get; set; }
+        public DateTime? Date { get; set; }
+        public string Description { get; set; }
     }
 }
