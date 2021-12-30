a1e26b27e31df4899d4de17342b4ec55f1a65498
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-30 17:15:10 -0600

    Core.CustomEntities PagedList<T> (agrega atributos basicos de la paginacion)

--- SocialMedia.Core/CustomEntities/PagedList.cs
+++ SocialMedia.Core/CustomEntities/PagedList.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,16 @@ namespace SocialMedia.Core.CustomEntities
 {
     public class PagedList<T> : List<T>
     {
+        public int CurrentPage { get; set; }    //Pagina actual 
+        public int TotalPages { get; set; }     //Total de paginas
+        public int PageSize { get; set; }       //tamaño de pagina (cantidad de registros a mostrar x pag)
+        public int TotalCount { get; set; }     //total de registros que hay
+
+        public bool HasPreviousPage => CurrentPage > 1;      //validar si hay pagina anterior 
+        public bool HasNextPage => CurrentPage < TotalPages;        //validar si hay pagina siguiente
+
+        public int? PreviousPageNumber => HasPreviousPage ? CurrentPage - 1: (int?) null;      //validar si hay pagina anterior 
+        public int? NextPageNumber => HasNextPage ? CurrentPage + 1: (int?) null;        //validar si hay pagina siguiente
+
     }
 }
