75b6565f443b014706de1f0558c767aeec572bad
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-31 14:00:54 -0600

    Core.CustomEntities Metadata (agregando propiedades)

--- SocialMedia.Core/CustomEntities/Metadata.cs
+++ SocialMedia.Core/CustomEntities/Metadata.cs
@@ -2,6 +2,15 @@
 {
     public class Metadata
     {
+        public int TotalCount { get; set; }
+        public int PageSize { get; set; }       //tamaño de pagina (cantidad de registros a mostrar x pag)
+        public int CurrentPage { get; set; }    //Pagina actual 
+        public int TotalPages { get; set; }     //Total de paginas
+        public bool HasNextPages { get; set; }
+        public bool HasPreviousPages { get; set; }
+
+        public string NextPageUrl { get; set; }
+        public string PreviousPageUrl { get; set; }
 
     }
 }
