9ff3168f4865765f23d7f920fcbe7006ea374992
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2021-12-31 16:55:24 -0600

    Infraestructure.Services UrlService (inicia clase y constructor)

---
+++ SocialMedia.Infraestructure/Services/UrlService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialMedia.Infraestructure.Services
+{
+    public class UrlService
+    {
+        public UrlService()
+        {
+
+        }
+    }
+}
