d427880c226053e041c7f0e3eb378486d6b5f67c
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2022-01-02 17:55:27 -0600

    Core.CustomEmtities PaginationOptions (agrega propiedades)

--- SocialMedia.Core/CustomEntities/PaginationOptions.cs
+++ SocialMedia.Core/CustomEntities/PaginationOptions.cs
@@ -6,6 +6,7 @@ namespace SocialMedia.Core.CustomEntities
 {
     public class PaginationOptions
     {
-    
+        public int DefaultPageSize { get; set; }
+        public int DefaultPageNumber { get; set; }     
     }
 }
