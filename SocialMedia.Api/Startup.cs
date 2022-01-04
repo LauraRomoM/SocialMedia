9d214dfcfa7f0cacce3042a042dc956353b5fa4b
Laura Romo M <laura.rmunoz@alumnos.udg.mx>
2022-01-04 10:55:12 -0600

    Api Startup Configure() (agrega generador de Interfaz de Ususario y posiciona en raiz de la API)

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -89,7 +89,7 @@ namespace SocialMedia.Api
             app.UseSwagger();
             app.UseSwaggerUI(options =>
             {
-                options.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Social Media API v1.0");          //colocamos url para que nuestro archivo swagger se vea con interfaz grafica, despues se coloca un nombre en este caso (Social Media API)
+                options.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Social Media API v1.0");          //colocamos url para generar interfaz grafica de nuestro archivo swagger.json, despues se coloca un nombre en este caso (Social Media API)
                 options.RoutePrefix = string.Empty;                 //basicamente para que tome la raiz de nuestra API
             });
 
